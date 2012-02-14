86b63557eb56898fdfacb9295f16aed88951f086
Alexander <alexander.skoglund@gmail.com>
2012-02-14 10:39:15 +0100

    first commit

---
+++ src/hello.cc
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+#include "../libkicylab/hellolib.h"
+
+
+/*void hellofunc(int n)
+{
+  int i;
+  for(i = 0; i < n; i++)
+    {
+      printf("Hello %d\n",i);
+    }
+    }*/
+
+int main(int argc, char *argv[])
+{
+
+  hellofunc(5);
+
+}
